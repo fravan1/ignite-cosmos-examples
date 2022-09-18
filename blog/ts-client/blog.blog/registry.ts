c26190463e694cdfb4910e8df45f61dd9b2868c8
Fatih <fatihfurkn@gmail.com>
2022-09-18 15:54:52 +0300

    scaffold create-comment message

--- blog/ts-client/blog.blog/registry.ts
+++ blog/ts-client/blog.blog/registry.ts
@@ -1,8 +1,10 @@
 import { GeneratedType } from "@cosmjs/proto-signing";
 import { MsgCreatePost } from "./types/blog/tx";
+import { MsgCreateComment } from "./types/blog/tx";
 
 const msgTypes: Array<[string, GeneratedType]>  = [
     ["/blog.blog.MsgCreatePost", MsgCreatePost],
+    ["/blog.blog.MsgCreateComment", MsgCreateComment],
     
 ];
 
