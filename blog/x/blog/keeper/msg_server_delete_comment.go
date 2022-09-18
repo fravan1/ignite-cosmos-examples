b0ee71af3cc9a64f7a995ff4fb92cc68e29cbb42
Fatih <fatihfurkn@gmail.com>
2022-09-18 16:19:49 +0300

    implement delete-comment message

--- blog/x/blog/keeper/msg_server_delete_comment.go
+++ blog/x/blog/keeper/msg_server_delete_comment.go
@@ -2,16 +2,30 @@ package keeper
 
 import (
 	"context"
+	"encoding/binary"
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 
 	"blog/x/blog/types"
+	coserrors "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) DeleteComment(goCtx context.Context, msg *types.MsgDeleteComment) (*types.MsgDeleteCommentResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	comment, exist := k.GetComment(ctx, msg.CommentID)
+	if !exist {
+		return nil, coserrors.Wrapf(types.ErrID, "Comment doesn't exist")
+	}
+
+	if msg.PostID != comment.PostID {
+		return nil, coserrors.Wrapf(types.ErrID, "Post Blog ID does not exist for which comment with Blog Id %d was made", msg.PostID)
+	}
+
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.CommentKey))
+	bz := make([]byte, 8)
+	binary.BigEndian.PutUint64(bz, comment.Id)
+	store.Delete(bz)
 
 	return &types.MsgDeleteCommentResponse{}, nil
 }
