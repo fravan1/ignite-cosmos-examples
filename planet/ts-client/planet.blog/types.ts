b612a5ea43acb31313b3056cf69f9a8c9c3cb2bf
Fatih <fatihfurkn@gmail.com>
2022-09-21 11:41:58 +0300

    ibc: ibcPost packet scaffolded

--- planet/ts-client/planet.blog/types.ts
+++ planet/ts-client/planet.blog/types.ts
@@ -1,5 +1,7 @@
 import { BlogPacketData } from "./types/blog/packet"
 import { NoData } from "./types/blog/packet"
+import { IbcPostPacketData } from "./types/blog/packet"
+import { IbcPostPacketAck } from "./types/blog/packet"
 import { Params } from "./types/blog/params"
 import { Post } from "./types/blog/post"
 import { SentPost } from "./types/blog/sent_post"
@@ -9,6 +11,8 @@ import { TimedoutPost } from "./types/blog/timedout_post"
 export {     
     BlogPacketData,
     NoData,
+    IbcPostPacketData,
+    IbcPostPacketAck,
     Params,
     Post,
     SentPost,
