9331da5dc8753f27edc93c39ecbe2d6f66947612
Fatih <fatihfurkn@gmail.com>
2022-09-21 11:38:25 +0300

    ibc: timedoutPost list scaffolded

--- planet/ts-client/planet.blog/types.ts
+++ planet/ts-client/planet.blog/types.ts
@@ -3,6 +3,7 @@ import { NoData } from "./types/blog/packet"
 import { Params } from "./types/blog/params"
 import { Post } from "./types/blog/post"
 import { SentPost } from "./types/blog/sent_post"
+import { TimedoutPost } from "./types/blog/timedout_post"
 
 
 export {     
@@ -11,5 +12,6 @@ export {
     Params,
     Post,
     SentPost,
+    TimedoutPost,
     
  }
