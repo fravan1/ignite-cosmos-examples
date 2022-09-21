256056eaf1b09ee8a9dea882f9a18d3308474999
Fatih <fatihfurkn@gmail.com>
2022-09-21 11:36:57 +0300

    ibc: post list scaffolded

--- planet/ts-client/planet.blog/types.ts
+++ planet/ts-client/planet.blog/types.ts
@@ -1,11 +1,13 @@
 import { BlogPacketData } from "./types/blog/packet"
 import { NoData } from "./types/blog/packet"
 import { Params } from "./types/blog/params"
+import { Post } from "./types/blog/post"
 
 
 export {     
     BlogPacketData,
     NoData,
     Params,
+    Post,
     
  }
